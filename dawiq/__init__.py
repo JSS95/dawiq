a04a98c656d30ad27b00b4831c2e9d06d6afd46a
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-07-20 13:44:53 +0900

    maint: add setup.py and necessary files

---
+++ dawiq/__init__.py
@@ -0,0 +1,9 @@
+"""
+DaWiQ - Dataclass Widget for Qt
+===============================
+
+DaWiQ is a package to build widgets from Python dataclasses.
+
+"""
+
+from .version import __version__  # noqa
---
+++ setup.py
@@ -0,0 +1,90 @@
+from itertools import chain
+import os
+from setuptools import setup, find_packages  # type: ignore[import]
+
+
+VERSION_FILE = "dawiq/version.py"
+
+
+def get_version():
+    with open(VERSION_FILE, "r") as f:
+        exec(compile(f.read(), VERSION_FILE, "exec"))
+    return locals()["__version__"]
+
+
+def read_readme():
+    with open("README.md", encoding="utf-8") as f:
+        content = f.read()
+    return content
+
+
+def get_package_data():
+    pkgname = "dawiq"
+    pkg_datapaths = ["py.typed",]
+
+    ret = []
+    for path in pkg_datapaths:
+        fullpath = os.path.join(pkgname, path)
+        if os.path.isfile(fullpath):
+            ret.append(path)
+        else:
+            for root, dirs, files in os.walk(fullpath):
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+                    ret.append(os.path.join(*filepath.split(os.sep)[1:]))
+
+    return {pkgname: ret}
+
+
+def read_requirements(path):
+    with open(path, "r") as f:
+        ret = f.read().splitlines()
+    return ret
+
+
+def get_extras_require():
+    ret = {}
+
+    ret["test"] = read_requirements("requirements/test.txt")
+    ret["doc"] = read_requirements("requirements/doc.txt")
+    ret["full"] = list(set(chain(*ret.values())))
+
+    return ret
+
+
+setup(
+    name="dawiq",
+    version=get_version(),
+    python_requires=">=3.9",
+    description="Package to construct widgets from dataclasses",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        (
+            "License :: OSI Approved :: "
+            "GNU Library or Lesser General Public License (LGPL)"
+        ),
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: Software Development :: Widget Sets",
+    ],
+    author="Jisoo Song",
+    author_email="jeesoo9595@snu.ac.kr",
+    maintainer="Jisoo Song",
+    maintainer_email="jeesoo9595@snu.ac.kr",
+    url="https://github.com/JSS95/dawiq",
+    license="LGPL",
+    packages=find_packages(),
+    package_data=get_package_data(),
+    install_requires=read_requirements("requirements/install.txt"),
+    extras_require=get_extras_require(),
+)
