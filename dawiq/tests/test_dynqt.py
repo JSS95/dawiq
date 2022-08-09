33bfe891c2983bef6041fe5fb91fe087da9f128e
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-08-09 10:53:18 +0900

    maint: add test

--- dawiq/tests/test_dynqt.py
+++ dawiq/tests/test_dynqt.py
@@ -1,12 +1,11 @@
-def test_import_QtCore():
-    from dawiq.dynqt import QtCore
+from dawiq.dynqt import QtCore, QtWidgets
 
+
+def test_import_QtCore():
     assert hasattr(QtCore, "Qt")
     assert hasattr(QtCore, "Signal")
     assert hasattr(QtCore, "Slot")
 
 
 def test_import_QtWidgets():
-    from dawiq.dynqt import QtWidgets
-
     assert hasattr(QtWidgets, "QWidget")
--- dawiq/tests/test_fieldwidgets.py
+++ dawiq/tests/test_fieldwidgets.py
@@ -1,5 +1,41 @@
-from dawiq.dynqt import QtWidgets
+from dawiq import BoolCheckBox
+from dawiq.dynqt import QtCore
 
 
-def test_construction(qtbot):
-    QtWidgets.QCheckBox()
+def test_BoolCheckBox(qtbot):
+    widget = BoolCheckBox()
+
+    # test dataValueChanged signal
+    with qtbot.waitSignal(
+        widget.dataValueChanged,
+        raising=True,
+        check_params_cb=lambda val: val is True,
+    ):
+        widget.setCheckState(QtCore.Qt.Checked)
+    with qtbot.waitSignal(
+        widget.dataValueChanged,
+        raising=True,
+        check_params_cb=lambda val: val is False,
+    ):
+        widget.setCheckState(QtCore.Qt.Unchecked)
+
+    # test tristate
+    widget.setTristate(True)
+    with qtbot.waitSignal(
+        widget.dataValueChanged,
+        raising=True,
+        check_params_cb=lambda val: val is True,
+    ):
+        widget.setCheckState(QtCore.Qt.Checked)
+    with qtbot.waitSignal(
+        widget.dataValueChanged,
+        raising=True,
+        check_params_cb=lambda val: val is False,
+    ):
+        widget.setCheckState(QtCore.Qt.Unchecked)
+    with qtbot.waitSignal(
+        widget.dataValueChanged,
+        raising=True,
+        check_params_cb=lambda val: val is None,
+    ):
+        widget.setCheckState(QtCore.Qt.PartiallyChecked)
